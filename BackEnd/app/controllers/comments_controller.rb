5327c7e1e6b8b855f0e6fc659401b39a17600f7b
David Garcia <davidjosegarcia12@gmail.com>
2021-06-22 23:52:36 -0500

    Se agregraron las validaciones

--- BackEnd/app/controllers/comments_controller.rb
+++ BackEnd/app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+    def index
+        comentarios = Comment.all
+        render json: comentarios, status: 200  
+    end
+
+    def create
+    comment = Comment.create(
+      nombre: params[:nombre],
+      correo: params[:correo],
+      edad: params[:edad]
+  )
+    render json: comment, status: 201
+    end
 end
--- BackEnd/app/models/comment.rb
+++ BackEnd/app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+    validates :nombre, :correo, :edad, presence: true
+    validates :email, uniqueness: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 end
